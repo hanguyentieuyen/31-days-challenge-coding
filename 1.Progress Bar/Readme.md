03f3c944caa2e7a4e6ba3b2f124a45ec0ad4cc90
yen <hanguyentieuyen@gmail.com>
2020-07-27 14:09:32 +0700

    day2 radom jokes

--- 1.Progress Bar/Readme.md
+++ 1.Progress Bar/Readme.md
@@ -1,4 +1,5 @@
 31 Days Challenge Coding
 -----------------------------
+1. Progress Bar
 -Tutorial from youtube channel Florin Pop
 -Learn HTML CSS JS 
---
+++ 2.RadomJokes/radomjokes.js
@@ -0,0 +1,17 @@
+const jokeEl = document.getElementById('joke');
+const get_joke = document.getElementById('get_joke');
+get_joke.addEventListener('click',generateJoke);
+generateJoke();
+async function generateJoke(){
+  //call api
+  const jokeRes = await fetch('https://icanhazdadjoke.com/',{
+    headers:{
+      'Accept':'application/json'
+    }
+  });
+  const joke = await jokeRes.json();
+  
+  //set the new joke
+  jokeEl.innerHTML = joke.joke;
+}
+
