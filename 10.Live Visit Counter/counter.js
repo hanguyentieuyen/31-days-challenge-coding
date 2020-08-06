a84ea46b37d93472a2ad240b30b0d64a3213752f
yen <hanguyentieuyen@gmail.com>
2020-08-06 21:01:41 +0700

    Day 10 Basic Fetch API

---
+++ 10.Live Visit Counter/counter.js
@@ -0,0 +1,15 @@
+// fetch api : https://countapi.xyz/
+// https://www.npmjs.com/package/countapi-js
+const countEl = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount() {
+  //Tạo một request network bằng fetch
+	fetch('https://api.countapi.xyz/update/youtube.com/?amount=1')
+  
+	.then(res => res.json())    //respone giá trị json
+	.then(res => {
+		countEl.innerHTML = res.value;  // lấy value để add vào id
+	})
+}
